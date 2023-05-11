eda5b42c8380e08754cea5c2a746a92a6cbf74cb
Juan <juan.gallego223@gmail.com>
2023-05-11 15:54:49 -0500

    Ajuste crud consultas

--- front/src/app/components/citas/edit-cita/edit-cita.component.ts
+++ front/src/app/components/citas/edit-cita/edit-cita.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ConsultaService } from 'src/app/services/consulta.service';
 import { RequestsService } from 'src/app/services/requests.service';
@@ -12,25 +13,37 @@ import { RequestsService } from 'src/app/services/requests.service';
 export class EditCitaComponent implements OnInit {
 
   consulta : any = {};
+  id : any;
 
   constructor(private _messageService: MessageService,
-              private _requestService: RequestsService){}
+              private _requestService: RequestsService,
+              private _route: ActivatedRoute,
+              private _router: Router){}
 
   
   ngOnInit(): void {
-    
+    this.initData();
   }
 
   actualizarConsulta(form: NgForm){
     if(form.valid){
-      console.log(this.consulta);      
+      this._requestService.put(`actualizarCita/${this.id}`, this.consulta).subscribe(resp => {
+        this._messageService.add({ severity: 'success', summary: resp.mensaje, detail: 'Cliente actualizado con exito!' });
+        this._router.navigateByUrl('/consultas/inicio');
+      });
     } else {
       this._messageService.add({ severity: 'error', summary: 'Error', detail: 'Todos los campos son requeridos' });
     }
   }
 
   initData(){
-    // this._requestService.get()
+    this._route.params.subscribe(params => {
+      this.id = params['id'];      
+    });
+    this._requestService.get(`obtenerCita/${this.id}`).subscribe(resp => {
+      console.log(resp);
+      this.consulta = resp.datos;
+    });
   }
 
 
