d1d2823380ca3aa86c5d75350be74ac29d13c010
luanquidi11 <lquinones965@gmail.com>
2023-05-17 23:14:13 -0500

    no message

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   nameSite: 'Óptica Kristal',
-  url: 'https://api.opticakristal.com/api/',
+  url: 'https://opticakristal.com/api/',
   urlFront: 'https://opticakristal.com/#/',
 };
